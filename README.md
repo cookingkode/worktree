7a5ac0c63ecabf585e10d976eaa56cc534bd47f2
jyotiswarup.raiturkar <jyotiswarup.raiturkar@goibibo.com>
2015-06-23 10:09:10 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # worktree
 Hierarchical, concurrent MapReduce framework 
+============================================
+
+Create a nested work tree  where each node has mapper child functions and a reducer function. All child map functions are executed concurrently in goroutines, and the reducer is called once all the mappers finish.
+
+Inspired by https://github.com/goibibo/lithosphere/blob/master/tree.go
+
+**Usage:**
+
+
+// two level
+func leaf2(i interface{}) interface{} {
+    args := i.(TwoArgs)
+
+    return args.X * args.Y
+}
+
+func merge2(results []interface{}) interface{} {
+    var sum int
+    for _, x := range results {
+        sum += x.(int)
+    }
+    return sum
+}
+
+func leaf1(i interface{}) interface{} {
+    args := i.(TwoArgs)
+
+    return args.X + args.Y
+}
+
+func main() {
+
+    // TWO Level
+
+    l2 := li.CommandTree{}
+    l2.AddMapper(leaf2, TwoArgs{2, 3})
+    l2.AddMapper(leaf2, TwoArgs{2, 2})
+    l2.AddReducer(merge2)
+
+    l1 := li.CommandTree{}
+    l1.AddMapper(l2.Run, nil)
+    l1.AddMapper(leaf2, TwoArgs{2, 2})
+    l1.AddReducer(merge2)
+
+    fmt.Println(l1.Run(nil).(int))
+
+}
---
+++ worktree.go
@@ -0,0 +1,71 @@
+package worktree
+
+import (
+	"log"
+	"time"
+)
+
+type CommandTree struct {
+	Reducer             func(inp []interface{}) interface{}
+	LeafFunctions       []func(inp interface{}) interface{}
+	LeafFunctionsInput  []interface{}
+	nChildren           int
+	LeafFunctionsOutput []interface{}
+}
+
+type LeafFunc func(inp interface{}) interface{}
+
+func (t *CommandTree) AddMapper(f func(inp interface{}) interface{}, input interface{}) int {
+
+	t.LeafFunctions = append(t.LeafFunctions, f)
+	t.LeafFunctionsInput = append(t.LeafFunctionsInput, input)
+	temp := t.nChildren
+	t.nChildren += 1
+	return temp
+}
+
+func (t *CommandTree) AddReducer(f func(inp []interface{}) interface{}) {
+	t.Reducer = f
+}
+
+//type Inputs map[string]interface{}
+
+type ResultFunction struct {
+	Child int
+	//Result reflect.Value
+	Result interface{}
+}
+
+func (t *CommandTree) Run(_ interface{}) interface{} {
+	// Execcute the tree
+
+	channel := make(chan ResultFunction, t.nChildren)
+	defer close(channel)
+
+	for i, f := range t.LeafFunctions {
+		go wrap(channel, i, f, t.LeafFunctionsInput[i])
+	}
+
+	remaining := t.nChildren
+	for remaining > 0 {
+		result := <-channel
+		remaining -= 1
+		t.LeafFunctionsOutput = append(t.LeafFunctionsOutput, result.Result)
+	}
+	return t.Reducer(t.LeafFunctionsOutput)
+
+}
+
+func wrap(c chan ResultFunction, child int, todo func(inp interface{}) interface{}, inp interface{}) {
+
+	var result ResultFunction
+
+	startTime := time.Now()
+	result.Result = todo(inp)
+	endTime := time.Now()
+	log.Println("WRAP TOTAL ", endTime.Sub(startTime))
+	result.Child = child
+
+	c <- result
+
+}
